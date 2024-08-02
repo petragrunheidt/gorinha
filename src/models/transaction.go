b17766676c96efb78ae291ba914d992f5da12c06
petra <petra.grunheidt@rebase.com.br>
2024-08-02 15:43:45 -0300

    adds validation for transactions

--- src/models/transaction.go
+++ src/models/transaction.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"errors"
 	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -10,6 +12,18 @@ type Transaction struct {
 	AccountID       uint
 	Amount          float64 `gorm:"not null"`
 	TransactionType string `gorm:"not null"`
-	Description     string
+	Description     string `gorm:"not null"`
 	Date            time.Time
+}
+
+func(t *Transaction) BeforeSave(tx *gorm.DB) (err error) {
+	if len(t.Description) > 10 || len(t.Description) < 1 {
+		return errors.New("description must have between 1 and 10 characters")
+	}
+
+	if t.TransactionType != "c" && t.TransactionType != "d" {
+		return errors.New("transaction type must be either 'c' or 'd'")
+	}
+
+	return nil
 }
